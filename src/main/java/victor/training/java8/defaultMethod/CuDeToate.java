e7dd0301751c643792c69716ad3c253381746cde
Victor Rentea <victorrentea@gmail.com>
2019-02-26 14:37:55 +0200

    Default methods - more

--- src/main/java/victor/training/java8/defaultMethod/CuDeToate.java
+++ src/main/java/victor/training/java8/defaultMethod/CuDeToate.java
@@ -4,14 +4,16 @@ import static java.util.Arrays.asList;
 
 import java.util.List;
 
-public class CuDeToate implements A {
-
+public class CuDeToate implements A,B {
 	public void m() {
 		System.out.println("NU INCERCATI ASTA ACASA!");
 	}
-
 	public void intrare() {
 		altaMetodaCuImplementare();
+	}
+	public void altaMetodaCuImplementare() {
+		A.super.altaMetodaCuImplementare(); // porceala!
+		// alegi catre care delegi, A sau B.
 	}
 	public static void main(String[] args) {
 		List<Integer> list = asList(1,2,3);
@@ -19,11 +21,18 @@ public class CuDeToate implements A {
 		new CuDeToate().intrare();
 	}
 }
-
+class D extends CuDeToate implements A {
+	// prefera metoda din superclasa in fata interfetei
+}
 interface A {
 	void m();
 	default void altaMetodaCuImplementare() {
 		System.out.println("DA! Cod in intefete!");
 		m();
+	}
+}
+interface B {
+	default void altaMetodaCuImplementare() {
+		System.out.println("DA! Cod in intefata B!");
 	}
 }
