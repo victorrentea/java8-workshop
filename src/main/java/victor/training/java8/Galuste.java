838164e1e399e64f602ffee6fa7d0419bad232ba
Victor Rentea <victorrentea@gmail.com>
2019-10-29 16:38:13 +0200

    Exceptionally --- exceptii

--- src/main/java/victor/training/java8/Galuste.java
+++ src/main/java/victor/training/java8/Galuste.java
@@ -10,6 +10,7 @@ import java.util.function.Supplier;
 import static java.util.concurrent.CompletableFuture.runAsync;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
 
+@Slf4j
 public class Galuste {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
@@ -21,8 +22,19 @@ public class Galuste {
         CompletableFuture<String> futurea1 =  futurea.thenApplyAsync(a -> Galusca3.m(a));
         // join
         futureA.thenCombineAsync(futurea1, (A,a1) -> A+a1)
+                .exceptionally(e -> {
+                    if (e.getMessage().contains("ORA-131421")) {
+                        throw new RuntimeException(e);
+                    } else {
+                        return "default";
+                    }
+                })
             .thenApplyAsync(Aa1 -> Galusca4.m(Aa1))
-            .thenAcceptAsync(Aa1Aa1 -> Galusca5.m(Aa1Aa1));
+            .thenAcceptAsync(Aa1Aa1 -> Galusca5.m(Aa1Aa1))
+        .exceptionally(e -> {
+            log.error(e.getMessage(),e);
+            return null;
+        });
 
         long t1 = System.currentTimeMillis();
         System.out.println(t1 - t0);
@@ -56,6 +68,7 @@ class Galusca3 {
     public static String m(String a) {
         log.debug("Procesez");
         Thread.sleep(1000);
+        if (true) throw new IllegalArgumentException("in gat");
         return a + "1";
     }
 }
