fcadc8d8346d022853b108be3d72920292c68939
Victor Rentea <victorrentea@gmail.com>
2020-06-12 11:12:59 +0300

    Am submis 2 taskuri paralele pe 2 threaduri dupa bere si tuica. Le astept sa termine pe amandoua, apoi savurez

--- src/main/java/victor/training/java8/parallel/Bar.java
+++ src/main/java/victor/training/java8/parallel/Bar.java
@@ -3,8 +3,11 @@ package victor.training.java8.parallel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 public class Bar {
-   public static void main(String[] args) {
+   public static void main(String[] args) throws ExecutionException, InterruptedException {
       Bautor.bea();
    }
 }
@@ -12,24 +15,33 @@ public class Bar {
 class Bautor {
    private static final Logger log = LoggerFactory.getLogger(Bautor.class);
 
-   public static void bea() {
+   public static void bea() throws ExecutionException, InterruptedException {
       log.info("Vreau sa beau");
       // PREREQ:
       // sunt reordonabile fara compilation sau functional failures
       // sunt thread-safe : pot fi chemate in paralel
-      Bere bere = Barman.toarnaBere();
-      Tuica tuica = Barman.toarnaTuica();
+
+      // java7 style
+//      ExecutorService executorService = Executors.newFixedThreadPool(3);
+//      Future<bere>  = executorService.submit(Barman::toarnaBere);
+
+      CompletableFuture<Bere> visDeBere = CompletableFuture.supplyAsync(() -> Barman.toarnaBere());
+      CompletableFuture<Tuica> visDeTuica = CompletableFuture.supplyAsync(Barman::toarnaTuica);
+      log.info("A plecat fata cu comanda");
+      Bere bere = visDeBere.get();
+      Tuica tuica = visDeTuica.get();
+//      Bere bere = Barman.toarnaBere();
+//      Tuica tuica = Barman.toarnaTuica();
       log.info("Beau " + bere + " cu " + tuica);
    }
 }
 
 
+
 class Barman {
    private static final Logger log = LoggerFactory.getLogger(Barman.class);
-//   static int bauturiLivrate;
    public static Bere toarnaBere() {
       log.info("Torn bere");
-//      bauturiLivrate ++;
       try {
          Thread.sleep(1000);
       } catch (InterruptedException e) {
@@ -41,7 +53,6 @@ class Barman {
 
    public static Tuica toarnaTuica() {
       log.info("Torn tuica");
-//      bauturiLivrate = bauturiLivrate + 1; // nu mai face thread safe clasa
       try {
          Thread.sleep(1000);
       } catch (InterruptedException e) {
