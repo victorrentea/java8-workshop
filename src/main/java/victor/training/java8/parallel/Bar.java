d8b17a8e37da98498ee2b2e94a6a2fbd722c3f5c
Victor Rentea <victorrentea@gmail.com>
2020-06-12 10:54:57 +0300

    Conditii sa poti paraleliza

--- src/main/java/victor/training/java8/parallel/Bar.java
+++ src/main/java/victor/training/java8/parallel/Bar.java
@@ -14,6 +14,9 @@ class Bautor {
 
    public static void bea() {
       log.info("Vreau sa beau");
+      // PREREQ:
+      // sunt reordonabile fara compilation sau functional failures
+      // sunt thread-safe : pot fi chemate in paralel
       Bere bere = Barman.toarnaBere();
       Tuica tuica = Barman.toarnaTuica();
       log.info("Beau " + bere + " cu " + tuica);
@@ -23,9 +26,10 @@ class Bautor {
 
 class Barman {
    private static final Logger log = LoggerFactory.getLogger(Barman.class);
-
+//   static int bauturiLivrate;
    public static Bere toarnaBere() {
       log.info("Torn bere");
+//      bauturiLivrate ++;
       try {
          Thread.sleep(1000);
       } catch (InterruptedException e) {
@@ -37,6 +41,7 @@ class Barman {
 
    public static Tuica toarnaTuica() {
       log.info("Torn tuica");
+//      bauturiLivrate = bauturiLivrate + 1; // nu mai face thread safe clasa
       try {
          Thread.sleep(1000);
       } catch (InterruptedException e) {
