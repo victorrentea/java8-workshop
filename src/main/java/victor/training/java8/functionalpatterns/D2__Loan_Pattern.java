5911c6cabe4b5c5437fc2c5040dc604650499113
Victor Rentea <victorrentea@gmail.com>
2020-07-02 13:00:49 +0300

    Implementezi primul export. Esti mandru de el. Ai transpirat la el

--- src/main/java/victor/training/java8/functionalpatterns/D2__Loan_Pattern.java
+++ src/main/java/victor/training/java8/functionalpatterns/D2__Loan_Pattern.java
@@ -2,9 +2,11 @@ package victor.training.java8.functionalpatterns;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Writer;
 import java.util.stream.Stream;
 
+import org.jooq.lambda.Unchecked;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // export all orders to a file
@@ -17,6 +19,21 @@ class OrderExporter {
 	private OrderRepo repo;
 
 
+	public void exportOrders(String fileName) {
+		File file = new File("/common/folder/path/" + fileName);
+		try (FileWriter writer = new FileWriter(file)) {
+			writer.write("id;creation_date\n");
+			repo.findByActiveTrue()
+				.map(order -> order.getId() + ";" + order.getCreationDate() + "\n")
+				.forEach(Unchecked.consumer(writer::write));
+		} catch (IOException e) {
+			// fac ceva inteligent
+			// Terror-Driven Development: o loghezi de frica. din neincredere in colegi.
+			// Nu ai incredere ca cineva ti-o prinde daca o rearunci
+			e.printStackTrace(); //sau log.debug(e.getMessage(), e);
+			throw new RuntimeException(e);
+		}
+	}
 
 
 }
