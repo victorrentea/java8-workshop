ade5dcfc757178be210c1ee74311364b4a470dd1
Victor Rentea <victorrentea@gmail.com>
2020-07-03 11:14:36 +0300

    workerii incearca sa treaca cate-un prin toti pasii by default, pentru a evita penalizari de sincronizare

--- src/main/java/victor/training/java8/stream/parallel/ParallelStreamPlay.java
+++ src/main/java/victor/training/java8/stream/parallel/ParallelStreamPlay.java
@@ -1,5 +1,8 @@
 package victor.training.java8.stream.parallel;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -7,14 +10,22 @@ import java.util.stream.IntStream;
 import static java.util.stream.Collectors.toList;
 
 public class ParallelStreamPlay {
+   private static final Logger log = LoggerFactory.getLogger(ParallelStreamPlay.class);
+
    public static void main(String[] args) {
       List<Integer> numbers = IntStream.range(1, 11) // IntStream
           .boxed() // Stream<Integer>
           .collect(toList());
 
       numbers.parallelStream()
-          .filter(n -> n % 2 == 1)
-          .map(n -> n * n)
-          .forEachOrdered(System.out::println);
+          .filter(n -> {
+             log.info("Filter " + n);
+             return n % 2 == 1;
+          })
+          .map(n -> {
+             log.info("Map " + n);
+             return n * n;
+          })
+          .forEach(System.out::println);
    }
 }
