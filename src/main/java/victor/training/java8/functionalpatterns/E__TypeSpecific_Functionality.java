62860caf13632153c02cf80f1884506731241025
Victor Rentea <victorrentea@gmail.com>
2020-07-03 13:05:05 +0300

    BiFunction in enum

--- src/main/java/victor/training/java8/functionalpatterns/E__TypeSpecific_Functionality.java
+++ src/main/java/victor/training/java8/functionalpatterns/E__TypeSpecific_Functionality.java
@@ -1,11 +1,19 @@
 package victor.training.java8.functionalpatterns;
 
+import java.util.function.BiFunction;
+
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class Movie {
 	enum Type {
-		REGULAR, NEW_RELEASE, CHILDREN
+		REGULAR(PriceService::computeRegularMoviePrice),
+		NEW_RELEASE(PriceService::computeNewReleaseMoviePrice),
+		CHILDREN(PriceService::computeChildrenMoviePrice);
+
+		public final BiFunction<PriceService, Integer, Integer> priceAlgo;
+		Type(BiFunction<PriceService, Integer, Integer> priceAlgo) {
+			this.priceAlgo = priceAlgo;
+		}
 	}
 
 	private final Type type;
@@ -14,24 +22,33 @@ class Movie {
 		this.type = type;
 	}
 
-	public int computePrice(int days) {
-		switch (type) {
-		case REGULAR:
-			return days + 1;
-		case NEW_RELEASE:
-			return days * 2;
-		case CHILDREN:
-			return 5;
-		}
-		return 0; // ?!.. Free!! Deducted from your salary!
+	public Type getType() {
+		return type;
+	}
+}
+
+class PriceService {
+	public int computeRegularMoviePrice(int days) {
+		return days + 1;
+	}
+	public int computeNewReleaseMoviePrice(int days) {
+		return days *2;
+	}
+	public int computeChildrenMoviePrice(int days) {
+		return 5;
+	}
+
+	public int computePrice(Movie movie, int days) {
+		return movie.getType().priceAlgo.apply(this, days);
 	}
 }
 
 public class E__TypeSpecific_Functionality {
 	public static void main(String[] args) {
-		System.out.println(new Movie(Movie.Type.REGULAR).computePrice(2));
-		System.out.println(new Movie(Movie.Type.NEW_RELEASE).computePrice(2));
-		System.out.println(new Movie(Movie.Type.CHILDREN).computePrice(2));
+		PriceService priceService = new PriceService();
+		System.out.println(priceService.computePrice(new Movie(Movie.Type.REGULAR), 2));
+		System.out.println(priceService.computePrice(new Movie(Movie.Type.NEW_RELEASE), 2));
+		System.out.println(priceService.computePrice(new Movie(Movie.Type.CHILDREN), 2));
 		System.out.println("COMMIT now!");
 	}
 }
