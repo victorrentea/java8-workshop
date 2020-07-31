dd834506454a2b33332b00fe99e9c351f85ed4fd
Victor Rentea <victorrentea@gmail.com>
2020-07-31 17:16:34 +0300

    Parallel Java7 style

--- src/main/java/victor/training/java8/promises/Bar.java
+++ src/main/java/victor/training/java8/promises/Bar.java
@@ -4,15 +4,26 @@ import lombok.SneakyThrows;
 import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 @Slf4j
 public class Bar {
-   public static void main(String[] args) {
+   public static void main(String[] args) throws ExecutionException, InterruptedException {
       Barman barman = new Barman();
 
       log.debug("Enter the BAR");
 
-      Beer beer = barman.pourBeer();
-      Rakia rakia = barman.pourRakia();
+      ExecutorService pool = Executors.newFixedThreadPool(2);
+
+      Future<Beer> futureBeer = pool.submit(barman::pourBeer);
+      Future<Rakia> futureRakia = pool.submit(barman::pourRakia);
+      log.debug("The waitress left with my order");
+
+      Beer beer = futureBeer.get();
+      Rakia rakia = futureRakia.get();
 
       log.debug("I am enjoying " + beer + " and " + rakia);
    }
@@ -22,11 +33,13 @@ public class Bar {
 class Barman {
    @SneakyThrows
    public Beer pourBeer() {
+      log.debug("Pouring beer");
       Thread.sleep(1000);
       return new Beer();
    }
    @SneakyThrows
    public Rakia pourRakia() {
+      log.debug("Pouring rakia");
       Thread.sleep(1000);
       return new Rakia();
    }
