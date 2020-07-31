be57b0a9df161ee021864cac228f8cf2790f3cee
Victor Rentea <victorrentea@gmail.com>
2020-07-31 17:45:49 +0300

    CompletableFutures

--- src/main/java/victor/training/java8/promises/Bar.java
+++ src/main/java/victor/training/java8/promises/Bar.java
@@ -1,9 +1,11 @@
 package victor.training.java8.promises;
 
+import lombok.Data;
 import lombok.SneakyThrows;
 import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Random;
 import java.util.concurrent.*;
 
 import static java.util.concurrent.CompletableFuture.supplyAsync;
@@ -17,19 +19,31 @@ public class Bar {
 
       ExecutorService pool = Executors.newFixedThreadPool(2);
 
+      ForkJoinPool ioOperationsPool = new ForkJoinPool(300);
+      // DeferredResult from spring will allow you to release the http thread once you configured the COmpletableFutures.
 
-      CompletableFuture<Beer> futureBeer = supplyAsync(barman::pourBeer);
-      CompletableFuture<Rakia> futureRakia = supplyAsync(barman::pourRakia);
+      CompletableFuture<Beer> futureBeer = supplyAsync(barman::pourBeer, ioOperationsPool);
+      CompletableFuture<Rakia> futureRakia = supplyAsync(barman::pourRakia, ioOperationsPool);
 
       log.debug("The waitress left with my order");
 
       // the http request thread is blocking here for 1 whole second
       // not serving any other incoming request
       // underuse
-      Beer beer = futureBeer.get();
-      Rakia rakia = futureRakia.get();
 
-      log.debug("I am enjoying " + beer + " and " + rakia);
+      CompletableFuture<Buzdugan> futureBuzdugan = futureBeer.thenCombineAsync(futureRakia, Buzdugan::new);
+
+      futureBuzdugan
+          .thenApplyAsync(b -> {
+             // CPU here
+             return b;
+          })
+          .thenApplyAsync(barman::addIce)
+          .thenAcceptAsync(b -> log.debug("I am enjoying " + b));
+      log.debug("exit the BAR");
+
+      Thread.sleep(2000); // hack for demo
+      // your app won't just die here.
    }
 }
 
@@ -38,7 +52,7 @@ class Barman {
    @SneakyThrows
    public Beer pourBeer() {
       log.debug("Pouring beer");
-      Thread.sleep(1000);
+      Thread.sleep(1000); // web service call, DB query
       return new Beer();
    }
    @SneakyThrows
@@ -47,6 +61,17 @@ class Barman {
       Thread.sleep(1000);
       return new Rakia();
    }
+   @SneakyThrows
+   public Buzdugan addIce(Buzdugan buzdugan) {
+      log.debug("AAdd ice");
+      Thread.sleep(1000);
+      return buzdugan;
+   }
 }
 class Beer{}
 class Rakia{}
+@Data
+class Buzdugan {
+   private final Beer beer;
+   private final Rakia rakia;
+}
---
+++ src/main/java/victor/training/java8/promises/MakeFunOfSwing.java
@@ -0,0 +1,4 @@
+package victor.training.java8.promises;
+
+public class MakeFunOfSwing {
+}
