3db8bbe3e265667c0fbed60fba430b63c600173c
Victor Rentea <victorrentea@gmail.com>
2020-07-31 17:59:55 +0300

    Try the next level of functional programmin in Java

--- src/main/java/victor/training/java8/stream/order/CreateStreams.java
+++ src/main/java/victor/training/java8/stream/order/CreateStreams.java
@@ -1,5 +1,6 @@
 package victor.training.java8.stream.order;
 
+import io.vavr.control.Try;
 import victor.training.java8.stream.order.entity.OrderLine;
 import victor.training.java8.stream.order.entity.Product;
 
@@ -7,6 +8,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
@@ -23,14 +25,20 @@ public class CreateStreams {
    public List<OrderLine> p1_readOrderFromFile(File file) throws IOException {
 
       try (Stream<String> lines = Files.lines(file.toPath())) {
-         return lines
+         List<Try<OrderLine>> allResults = lines
              .map(line -> line.split(";")) // Stream<String[]>
              .filter(cell -> "LINE".equals(cell[0]))
              .map(this::parseOrderLine) // Stream<OrderLine>
-             .peek(this::validateOrderLine)
+             .map(this::validateOrderLine)
 //             .forEach(repo::save);
 //             .forEach(messageSender::);
              .collect(toList());
+         System.out.println(allResults.stream()
+             .filter(Try::isFailure)
+             .map(Try::getCause)
+             .map(Throwable::getMessage)
+             .collect(Collectors.joining("\n")));
+         return allResults.stream().filter(Try::isSuccess).map(Try::get).collect(toList());
       }
    }
 
@@ -38,10 +46,12 @@ public class CreateStreams {
       return new OrderLine(new Product(cells[1]), Integer.parseInt(cells[2]));
    }
 
-   private void validateOrderLine(OrderLine orderLine) {
-      if (orderLine.getCount() < 0) {
-         throw new IllegalArgumentException("Negative items");
-      }
+   private Try<OrderLine> validateOrderLine(OrderLine orderLine) {
+      return Try.ofSupplier(() -> {
+         if (orderLine.getCount() < 0) {
+            throw new IllegalArgumentException("Negative items");
+         } else return orderLine;
+      });
    }
 
    public Stream<Integer> p2_createFibonacciStream() {
