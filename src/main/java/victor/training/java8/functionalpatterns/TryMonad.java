aa75c20eb165597ead1ecaac242c936a23e442c0
Victor Rentea <victorrentea@gmail.com>
2020-11-16 16:32:41 +0200

    Try monad (vavr) permite sa retii exceptia si sa continui parcurgerea elementelor tale.

--- src/main/java/victor/training/java8/functionalpatterns/TryMonad.java
+++ src/main/java/victor/training/java8/functionalpatterns/TryMonad.java
@@ -1,5 +1,6 @@
 package victor.training.java8.functionalpatterns;
 
+import io.vavr.control.Try;
 import org.jooq.lambda.Unchecked;
 
 import java.text.SimpleDateFormat;
@@ -16,8 +17,31 @@ public class TryMonad {
 
       List<String> dateStr = Arrays.asList("2020-10-11", "2020-nov-12", "2020-12-01");
       SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-      List<Date> dates = dateStr.stream().map(Unchecked.function(sdf::parse)).collect(Collectors.toList());
+//      List<Date> dates = dateStr.stream().map(Unchecked.function(sdf::parse)).collect(Collectors.toList());
 
+      // requirement: daca < 50% sunt gresite, accepta ce poti
+
+
+      List<Try<Integer>> tot = dateStr.stream()
+          .map(s -> parseIfPossible(sdf, s))
+          .map(dateTry -> dateTry.flatMap(TryMonad::throwingFunc))
+          .collect(Collectors.toList());
+
+      long failed = tot.stream().filter(Try::isFailure).count();
+      if (failed * 2 <  dateStr.size()) {
+         List<Integer> success = tot.stream().filter(Try::isSuccess).map(Try::get).collect(Collectors.toList());
+         System.out.println("Parsed: " + success);
+      }
+
+
+   }
+
+   private static Try<Integer> throwingFunc(Date date) {
+      return Try.success(date.getDate());
+   }
+
+   private static Try<Date> parseIfPossible(SimpleDateFormat sdf, String s) {
+      return Try.of(() -> sdf.parse(s));
    }
 
 //   interface FunctieRea<T, R> {
