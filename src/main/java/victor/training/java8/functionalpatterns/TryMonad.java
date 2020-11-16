e0242c9ad8661f76c47caa77e9870ceb9d6cecbf
Victor Rentea <victorrentea@gmail.com>
2020-11-16 16:20:03 +0200

    Unchecked (jool lib) pentru a converti checked in runtime pt noi

--- src/main/java/victor/training/java8/functionalpatterns/TryMonad.java
+++ src/main/java/victor/training/java8/functionalpatterns/TryMonad.java
@@ -1,14 +1,13 @@
 package victor.training.java8.functionalpatterns;
 
-import lombok.SneakyThrows;
+import org.jooq.lambda.Unchecked;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.zip.DataFormatException;
 
 public class TryMonad {
 
@@ -17,12 +16,27 @@ public class TryMonad {
 
       List<String> dateStr = Arrays.asList("2020-10-11", "2020-nov-12", "2020-12-01");
       SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-      List<Date> dates = dateStr.stream().map(s-> parseShit(sdf, s)).collect(Collectors.toList());
+      List<Date> dates = dateStr.stream().map(Unchecked.function(sdf::parse)).collect(Collectors.toList());
 
    }
 
-   @SneakyThrows
-   private static Date parseShit(SimpleDateFormat sdf, String s) {
-      return sdf.parse(s);
-   }
+//   interface FunctieRea<T, R> {
+//      R apply(T t) throws Throwable;
+//   }
+
+
+//   public static <T,R> Function<T,R> asRuntimeEx(FunctieRea<T,R> oFunctieRea) {
+//      return t -> {
+//         try {
+//            return oFunctieRea.apply(t);
+//         } catch (Throwable e) {
+//            throw new RuntimeException(e);
+//         }
+//      };
+//   }
+
+//   @SneakyThrows
+//   private static Date parseShit(SimpleDateFormat sdf, String s) {
+//      return sdf.parse(s);
+//   }
 }
