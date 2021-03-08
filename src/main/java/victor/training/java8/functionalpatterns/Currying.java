5c1ddda3c7801c0d791a35ca2f7af48e6ab4ed7b
Victor Rentea <victorrentea@gmail.com>
2021-03-08 13:23:55 +0200

    Daca te apuci de asa ceva, ai gresit limbajul

--- src/main/java/victor/training/java8/functionalpatterns/Currying.java
+++ src/main/java/victor/training/java8/functionalpatterns/Currying.java
@@ -10,14 +10,20 @@ public class Currying {
       BiFunction<Integer, Integer, Integer> sum = Integer::sum;
 
 
-      Function<Integer, Integer> adunaCu1 = partialApplySumOData(sum, 1);
+      Function<Integer, Integer> adunaCu1 = partialApplyOData(sum, 1);
 
       System.out.println(adunaCu1.apply(2));
+
+
+      Function<Integer, Integer> maxCu2 = partialApplyOData(Integer::max, 2);
+
+      System.out.println(maxCu2.apply(5));
+      System.out.println(maxCu2.apply(1));
 
 //      return a + b
    }
 
-   public static Function<Integer, Integer> partialApplySumOData(BiFunction<Integer, Integer, Integer> bi, int arg1) {
-      return arg2 -> arg1 + arg2;
+   public static Function<Integer, Integer> partialApplyOData(BiFunction<Integer, Integer, Integer> bi, int arg1) {
+      return arg2 -> bi.apply(arg1, arg2);
    }
 }
