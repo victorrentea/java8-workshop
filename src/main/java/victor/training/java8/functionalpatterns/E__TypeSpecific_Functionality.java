fde06e5c9e14b1517ce7086eeace0943b01c3bee
Victor Rentea <victorrentea@gmail.com>
2021-03-08 13:01:38 +0200

    am pus in enum valori (int) si functii *abstracte!*

--- src/main/java/victor/training/java8/functionalpatterns/E__TypeSpecific_Functionality.java
+++ src/main/java/victor/training/java8/functionalpatterns/E__TypeSpecific_Functionality.java
@@ -1,37 +1,53 @@
 package victor.training.java8.functionalpatterns;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-class Movie {
-	enum Type {
-		REGULAR, NEW_RELEASE, CHILDREN
+enum MovieType {
+	REGULAR(7) {
+		public int computePrice(int days) {
+			return days +1 ;
+		}
+	},
+	NEW_RELEASE(4) {
+		public int computePrice(int days) {
+			return days * 2;
+		}
+	},
+	CHILDREN(14) {
+		public int computePrice(int days) {
+			return 5;
+		}
+	};
+	private final int maxDays;
+	MovieType(int maxDays) {
+		this.maxDays = maxDays;
 	}
+	public int getMaxDays() {
+		return maxDays;
+	}
+	public abstract int computePrice(int days);
+}
+class Movie {
 
-	private final Type type;
+	private final MovieType type;
 
-	public Movie(Type type) {
+	public Movie(MovieType type) {
 		this.type = type;
 	}
 
 	public int computePrice(int days) {
-		switch (type) {
-		case REGULAR:
-			return days + 1;
-		case NEW_RELEASE:
-			return days * 2;
-		case CHILDREN:
-			return 5;
-		}
-		return 0; // ?!.. Free!! Deducted from your salary!
+		return type.computePrice(days);
+	}
+	public int maxNumberOfRentDays() {
+		return type.getMaxDays();
 	}
 }
 
 public class E__TypeSpecific_Functionality {
 	public static void main(String[] args) {
-		System.out.println(new Movie(Movie.Type.REGULAR).computePrice(2));
-		System.out.println(new Movie(Movie.Type.NEW_RELEASE).computePrice(2));
-		System.out.println(new Movie(Movie.Type.CHILDREN).computePrice(2));
+		System.out.println(new Movie(MovieType.REGULAR).computePrice(2));
+		System.out.println(new Movie(MovieType.NEW_RELEASE).computePrice(2));
+		System.out.println(new Movie(MovieType.CHILDREN).computePrice(2));
 		System.out.println("COMMIT now!");
 	}
 }
