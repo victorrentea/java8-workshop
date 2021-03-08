af7bcdfd5bcc05fd0f8be74ddd1a994fff2d871a
Victor Rentea <victorrentea@gmail.com>
2021-03-08 12:12:56 +0200

    Rearuncarea exceptiilor checked ca runtime printr-o functie higher-order

--- src/main/java/victor/training/java8/functionalpatterns/D2__Loan_Pattern.java
+++ src/main/java/victor/training/java8/functionalpatterns/D2__Loan_Pattern.java
@@ -2,9 +2,12 @@ package victor.training.java8.functionalpatterns;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Writer;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
+import org.jooq.lambda.Unchecked;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // export all orders to a file
@@ -15,14 +18,24 @@ interface OrderRepo extends JpaRepository<Order, Long> { // Spring Data FanClub
 class OrderExporter {
 	
 	private OrderRepo repo;
+
+
+	@FunctionalInterface
+	interface CheckedConsumer<T> {
+		void accept(T t) throws  Throwable;
+	}
+
 			
 	public File exportFile(String fileName) throws Exception {
 		File file = new File("export/" + fileName);
 		try (Writer writer = new FileWriter(file)) {
 			writer.write("OrderID;Date\n");
-//			repo.findByActiveTrue()
-//				.map(o -> o.getId() + ";" + o.getCreationDate())
-//				.forEach(writer::write);
+
+
+			repo.findByActiveTrue()
+				.map(o -> o.getId() + ";" + o.getCreationDate())
+//				.forEach(Unchecked.consumer(writer::write));
+				.forEach(convert(writer::write));
 			return file;
 		} catch (Exception e) {
 			// TODO send email notification
@@ -30,5 +43,20 @@ class OrderExporter {
 			throw e;
 		}
 	}
+
+	static private <T> Consumer<T> convert(CheckedConsumer<T> throwing) {
+		return s -> {
+			try {
+				throwing.accept(s);
+			} catch (Throwable e) {
+				throw new RuntimeException(e);
+			}
+		};
+	}
+
+
+//	public static
+
+
 }
 
