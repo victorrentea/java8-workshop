7262646b1778860d1a1cd6949e548da16e985f63
Victor Rentea <victorrentea@gmail.com>
2021-03-08 16:37:12 +0200

    prep

--- src/main/java/victor/training/java8/advanced/Exceptions.java
+++ src/main/java/victor/training/java8/advanced/Exceptions.java
@@ -1,19 +1,28 @@
 package victor.training.java8.advanced;
 
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class Exceptions {
 
    public static void main(String[] args) {
-      List<String> dateStr = Arrays.asList("2020-10-11", "2020-nov-12", "2020-12-01");
-      SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+      List<String> dates = List.of("2020-10-11", "2020-nov-12", "2020-12-01");
+      System.out.println(parseDates(dates));
+   }
 
-      // TODO jool
-      // List<Date> dates = dateStr.stream().map(
+      // TODO Accept if >= 50% valid; otherwise throw exception (Hint: average)
+   private static List<LocalDate> parseDates(List<String> dateStrList) {
+      DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-      // TODO vavr: Accept if parseable >= 50 (Hint: average)
+      List<LocalDate> dates = dateStrList.stream()
+          .map(text -> LocalDate.parse(text, pattern))
+          .collect(toList());
 
+      return dates;
    }
 }
