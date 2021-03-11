d86eda1194736b1f83551422d2195542c0ed1f7b
Victor Rentea <victorrentea@gmail.com>
2021-03-11 14:45:00 +0200

    Pauza1

--- src/main/java/victor/training/java8/stream/order/SearchStreams.java
+++ src/main/java/victor/training/java8/stream/order/SearchStreams.java
@@ -1,13 +1,17 @@
 package victor.training.java8.stream.order;
 
+import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 import victor.training.java8.stream.order.entity.Customer;
 import victor.training.java8.stream.order.entity.Order;
+import victor.training.java8.stream.order.entity.Order.Status;
+import victor.training.java8.stream.order.entity.OrderLine;
 
 public class SearchStreams {
 	
@@ -21,10 +25,10 @@ public class SearchStreams {
 	/**
 	 * - shorten/clean it up
 	 */
-	public List<Order> p1_getActiveOrders(Customer customer) {	
+	public List<Order> p1_getActiveOrders(Customer customer) {
 		return customer.getOrders().stream()
-				.filter(order -> order.getStatus() == Order.Status.ACTIVE)
-				.collect(toList());
+			.filter(Order::isActive)
+			.collect(toList());
 	}
 	
 	/**
@@ -32,22 +36,17 @@ public class SearchStreams {
 	 * - ...Any or ...First ?
 	 * - what do you do when you don't find it ? null/throw/Optional ?
 	 */
-	public Order p2_getOrderById(List<Order> orders, long orderId) {
+	public Optional<Order> p2_getOrderById(List<Order> orders, long orderId) {
 		return orders.stream()
-				.filter(order -> order.getId() == orderId)
-				.findFirst()
-				.orElse(null);
-				// you do elseThrow if you want a custom exception.
-				// .orElseThrow(() -> new MyException("There is no Order in the provided list that matches the id provided: " + orderId));
-				// Better, keeps the reader focused:
-//				.orElseThrow(() -> new MyException(MYErrorCode.NO_ORDER, orderId));
+			.filter(order -> order.getId() == orderId)
+			.findFirst();
 	}
 	
 	/**
 	 * @return true if customer has at least one order with status ACTIVE
 	 */
 	public boolean p3_hasActiveOrders(Customer customer) {
-		return true; 
+		return customer.getOrders().stream().anyMatch(Order::isActive);
 	}
 
 	/**
@@ -55,7 +54,7 @@ public class SearchStreams {
 	 * any OrderLine with isSpecialOffer()==true
 	 */
 	public boolean p4_canBeReturned(Order order) {
-		return true; // order.getOrderLines().stream() 
+		return order.getOrderLines().stream().noneMatch(OrderLine::isSpecialOffer);
 	}
 	
 	// ---------- select the best ------------
@@ -66,14 +65,23 @@ public class SearchStreams {
 	 * - Challenge: return an Optional<creationDate>
 	 */
 	public Order p5_getMaxPriceOrder(Customer customer) {
-		return null; 
+//		Comparator<Order> comparator = (o1, o2) -> o1.getTotalPrice().compareTo(o2.getTotalPrice());
+		return customer.getOrders().stream()
+			.max(comparing(Order::getTotalPrice))
+			.orElse(null);
 	}
 	
 	/**
+		// ORDER BY CREATION_DATE DESC LIMIT 3
 	 * last 3 Orders sorted descending by creationDate
 	 */
 	public List<Order> p6_getLast3Orders(Customer customer) {
-		return null; 
+		Comparator<Order> comparing = comparing(Order::getCreationDate);
+		return customer.getOrders()
+			.stream()
+			.sorted(comparing.reversed())
+			.limit(3)
+			.collect(toList());
 	}
 	
 	
--- src/main/java/victor/training/java8/stream/order/TransformStreams.java
+++ src/main/java/victor/training/java8/stream/order/TransformStreams.java
@@ -2,26 +2,15 @@ package victor.training.java8.stream.order;
 
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.summingLong;
-import static java.util.stream.Collectors.toCollection;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
-import static java.util.stream.Collectors.toSet;
 
-import java.io.File;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.nio.file.Files;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedSet;
-import java.util.TreeSet;
-import java.util.function.Function;
-import java.util.stream.Stream;
 
 import victor.training.java8.stream.order.dto.OrderDto;
 import victor.training.java8.stream.order.entity.Customer;
@@ -37,18 +26,18 @@ public class TransformStreams {
 	 * Discussion:.. Make it cleanest!
 	 */
 	public List<OrderDto> p01_toDtos(List<Order> orders) {
-		
-		List<OrderDto> dtos = new ArrayList<>();
-		for (Order order : orders) {
-			OrderDto dto = new OrderDto();
-			dto.totalPrice = order.getTotalPrice(); 
-			dto.creationDate = order.getCreationDate();
-			dtos.add(dto);
-		}
-		return dtos;
-		
+		return orders.stream()
+			.map(order -> toDto(order))
+			.collect(toList());
 	}
-	
+
+	private OrderDto toDto(Order order) {
+		OrderDto dto = new OrderDto();
+		dto.totalPrice = order.getTotalPrice();
+		dto.creationDate = order.getCreationDate();
+		return dto;
+	}
+
 	/**
 	 * Note: Order.getPaymentMethod()
 	 */
