46924cc7366df81b2e456cd02f0ed9515a7e3924
Victor Rentea <victorrentea@gmail.com>
2021-03-11 17:21:49 +0200

    flatMap

--- src/main/java/victor/training/java8/advanced/Optionals.java
+++ src/main/java/victor/training/java8/advanced/Optionals.java
@@ -3,29 +3,36 @@ package victor.training.java8.advanced;
 import victor.training.java8.advanced.model.Customer;
 import victor.training.java8.advanced.model.MemberCard;
 
-import java.io.BufferedReader;
-import java.io.Reader;
+import java.util.Optional;
 
 public class Optionals {
 	public static void main(String[] args) {
 		// test: 60, 10, no MemberCard
 		System.out.println(getDiscountLine(new Customer(new MemberCard(60))));
+		System.out.println(getDiscountLine(new Customer(new MemberCard(10))));
+		System.out.println(getDiscountLine(new Customer()));
 
 		//
 	}
 
 	public static String getDiscountLine(Customer customer) {
-		return "Discount: " + getApplicableDiscountPercentage(customer.getMemberCard());
+		return customer.getMemberCard()
+			.flatMap(card -> getApplicableDiscountPercentage(card))
+			.map(p -> "Discount: " + p)
+			.orElse("");
 	}
 
-	private static Integer getApplicableDiscountPercentage(MemberCard card) {
+	private static Optional<Integer> getApplicableDiscountPercentage(MemberCard card) {
+		if (card == null) {
+			return  Optional.empty();
+		}
 		if (card.getFidelityPoints() >= 100) {
-			return 5;
+			return Optional.of(5);
 		}
 		if (card.getFidelityPoints() >= 50) {
-			return 3;
+			return Optional.of(3);
 		}
-		return null;
+		return Optional.empty();
 	}
 }
 
--- src/main/java/victor/training/java8/advanced/model/Customer.java
+++ src/main/java/victor/training/java8/advanced/model/Customer.java
@@ -1,8 +1,11 @@
 package victor.training.java8.advanced.model;
 
+import java.util.Optional;
+
 // @Entity - The HOLY ENTITY !
 public class Customer {
    private MemberCard memberCard;
+//   private String name; // NOT NULL in DB
 
    public Customer() {
    }
@@ -11,7 +14,7 @@ public class Customer {
       this.memberCard = profile;
    }
 
-   public MemberCard getMemberCard() {
-      return memberCard;
+   public Optional<MemberCard> getMemberCard() {
+      return Optional.ofNullable(memberCard);
    }
 }
