3c3f7956d4d88dbf80e21241fdfecac36f414af7
Victor Rentea <victorrentea@gmail.com>
2021-03-12 11:06:14 +0200

    Referim functii statice dintr-un enum

--- src/main/java/victor/training/java8/advanced/FunctionsOnEnums.java
+++ src/main/java/victor/training/java8/advanced/FunctionsOnEnums.java
@@ -1,44 +1,53 @@
 package victor.training.java8.advanced;
 
 
-enum MovieType {
-   REGULAR{
-      public int computePrice(int days) {
-         // Problema cand e multa logica
-         return days + 1;
-      }
-   },
-   NEW_RELEASE {
-      public int computePrice(int days) {
-         // daca factorul "2" trebuie citit dintr-un fisier de proprietati ~ @Value
-         return days * 2;
-      }
-   },
-   CHILDREN {
-      public int computePrice(int days) {
-         return 5;
-      }
-   }
-   ,DE_BABACI {
-      public int computePrice(int days) {
-         return 1;
-      }
-   };
-   ;
-   public abstract int computePrice(int days);
+import java.text.SimpleDateFormat;
+import java.util.function.Function;
 
+
+class PriceCalculator {
+   public static int computeRegularPrice(int days) {
+      return days + 1;
+   }
+   public static int computeNewReleasePrice(int days) {
+      return days * 2;
+   }
+   public static int computeChildrenPrice(int days) {
+      return 5;
+   }
+}
+
+// 1. Problema cand e multa logica OK - cu referinte la metode statice din enum
+// daca factorul "2" trebuie citit dintr-un fisier de proprietati ~ @Value
+enum MovieType {
+   REGULAR(PriceCalculator::computeRegularPrice),
+   NEW_RELEASE(PriceCalculator::computeNewReleasePrice) ,
+   CHILDREN(PriceCalculator::computeChildrenPrice)
+   ;
+
+   private final Function<Integer, Integer> priceAlgo;
+
+   MovieType(Function<Integer, Integer> priceAlgo) {
+      this.priceAlgo = priceAlgo;
+   }
+
+   public Function<Integer, Integer> getPriceAlgo() {
+      return priceAlgo;
+   }
 }
 
 public class FunctionsOnEnums {
 
    public static void main(String[] args) {
+
+//      new SimpleDateFormat().parse("aa");
       System.out.println(computePrice(MovieType.REGULAR, 2));
       System.out.println(computePrice(MovieType.NEW_RELEASE, 2));
       System.out.println(computePrice(MovieType.CHILDREN, 2));
    }
 
    public static int computePrice(MovieType type, int days) {
-      return type.computePrice(days);
+      return type.getPriceAlgo().apply(days);
    }
 
 
