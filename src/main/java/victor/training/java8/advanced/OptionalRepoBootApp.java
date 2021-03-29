54cd33117894de631de3ba4bade487471b0519d3
Victor Rentea <victorrentea@gmail.com>
2021-03-29 11:41:31 +0300

    BREAK 2

--- src/main/java/victor/training/java8/advanced/OptionalRepoBootApp.java
+++ src/main/java/victor/training/java8/advanced/OptionalRepoBootApp.java
@@ -8,6 +8,12 @@ import org.springframework.transaction.annotation.Transactional;
 import victor.training.java8.advanced.model.Product;
 import victor.training.java8.advanced.repo.ProductRepo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.util.stream.Stream;
+
 @RequiredArgsConstructor
 @SpringBootApplication
 public class OptionalRepoBootApp implements CommandLineRunner {
@@ -16,13 +22,26 @@ public class OptionalRepoBootApp implements CommandLineRunner {
    }
    private final ProductRepo productRepo;
 
-   @Transactional(readOnly = true)
+   @Transactional//(readOnly = true)
    public void run(String... args) throws Exception {
       productRepo.save(new Product("Tree"));
 
       System.out.println(productRepo.findByNameContaining("re"));
       System.out.println(productRepo.findByNameContaining("rx"));
 
-      productRepo.streamAllByDeletedFalse().forEach(System.out::println);
+      try (Stream<Product> productStream = productRepo.streamAllByDeletedFalse()) {
+         productStream.forEach(System.out::println);
+      }
+
+
+//      Reader reader;
+//      try (BufferedReader bufferedReader = new BufferedReader(reader)) {
+//         bufferedReader.lines()
+//      }
+//      file.delete();
+//
+//      try (Stream<String> lines = Files.lines(new File("a.txt").toPath())) {
+//         lines.filter().map().forEach();
+//      }
    }
 }
