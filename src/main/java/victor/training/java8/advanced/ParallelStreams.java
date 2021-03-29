21eed2b1e192fe2b09bb53aea45d4650a97c7bbf
Victor Rentea <victorrentea@gmail.com>
2021-03-29 14:04:08 +0300

    END

--- src/main/java/victor/training/java8/advanced/ParallelStreams.java
+++ src/main/java/victor/training/java8/advanced/ParallelStreams.java
@@ -5,7 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import victor.training.java8.advanced.tricks.ConcurrencyUtil;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
@@ -13,24 +16,35 @@ import static victor.training.java8.advanced.tricks.ConcurrencyUtil.sleepq;
 
 @Slf4j
 public class ParallelStreams {
-   public static void main(String[] args) {
+   public static void main(String[] args) throws ExecutionException, InterruptedException {
 //      Enemy.parallelRequest();
 
       long t0 = System.currentTimeMillis();
 
-      List<Integer> list = asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+//      List<Integer> list = asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-      List<Integer> result = list.stream()
+      List<Integer> list = IntStream.range(1, 10_000).boxed().collect(toList());
+
+
+      Stream<Integer> stream = list.parallelStream()
           .filter(i -> {
              log.debug("Filter " + i);
              return i % 2 == 0;
           })
+//          .distinct()
+//          .sorted()
           .map(i -> {
              log.debug("Map " + i);
-             sleepq(100); // do some 'paralellizable' work
              return i * 2;
-          })
-          .collect(toList());
+          });
+
+
+      ForkJoinPool pool = new ForkJoinPool(200);
+
+      List<Integer> result =  pool.submit(() -> stream.collect(toList()) ).get();
+
+      // ThreadLocal (curret username, session, locale, tenantId)
+
       log.debug("Got result: " + result);
 
       long t1 = System.currentTimeMillis();
