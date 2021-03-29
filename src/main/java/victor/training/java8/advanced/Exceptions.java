b44506b63ba520f635c72c15d9c16a2de87b251b
Victor Rentea <victorrentea@gmail.com>
2021-03-29 13:26:21 +0300

    yolo

--- src/main/java/victor/training/java8/advanced/Exceptions.java
+++ src/main/java/victor/training/java8/advanced/Exceptions.java
@@ -1,11 +1,19 @@
 package victor.training.java8.advanced;
 
+import io.vavr.collection.Seq;
+import io.vavr.control.Either;
+import io.vavr.control.Try;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 public class Exceptions {
@@ -15,10 +23,34 @@ public class Exceptions {
    public List<LocalDate> parseDates(List<String> dateStrList) {
       DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-      List<LocalDate> dates = dateStrList.stream()
-          .map(text -> LocalDate.parse(text, pattern))
+      List<Try<LocalDate>> dateTries = dateStrList.stream()
+          .map(dateStr -> Try.of(() -> LocalDate.parse(dateStr, pattern)))
           .collect(toList());
+//
+//      Try<LocalDate> t = Try.ofSupplier(() -> LocalDate.now());
+//      Try<?> errorTry = t.flatMap(date -> riskyFunction(date));
+//      t.flatMapTry(date -> riskyFunction(date));
 
-      return dates;
+      double successRatio = dateTries.stream().mapToDouble(t -> t.isSuccess() ? 1 : 0).average().orElse(0);
+      if (successRatio < .5) {
+         String errors = dateTries.stream().filter(Try::isFailure).map(t -> t.getCause().getMessage()).collect(joining("\n"));
+         throw new IllegalArgumentException("Could not parse: " + errors);
+      }
+
+
+      return dateTries.stream().filter(Try::isSuccess).map(Try::get).collect(toList());
+   }
+
+//   private Try<?> riskyFunction(LocalDate date) {
+//   }
+
+
+   public void processLines(Supplier<Stream<String>> lineStream) {
+      try (Stream<String> lines = lineStream.get()) {
+         lines.forEach(System.out::println);
+      }
+      try (Stream<String> lines = lineStream.get()) {
+         lines.forEach(System.out::println);
+      }
    }
 }
