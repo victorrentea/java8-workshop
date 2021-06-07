4ce18218f7388c01b7db47da132968c599a10c09
Victor Rentea <victorrentea@gmail.com>
2021-06-07 12:20:26 +0300

    flatMap + groupBy(key, collector2)

--- src/main/java/victor/training/java8/stream/order/TransformStreams.java
+++ src/main/java/victor/training/java8/stream/order/TransformStreams.java
@@ -1,13 +1,5 @@
 package victor.training.java8.stream.order;
 
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.summingLong;
-import static java.util.stream.Collectors.toCollection;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
-import static java.util.stream.Collectors.toSet;
-
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -15,6 +7,7 @@ import java.nio.file.Files;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.function.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +18,8 @@ import victor.training.java8.stream.order.entity.Order;
 import victor.training.java8.stream.order.entity.OrderLine;
 import victor.training.java8.stream.order.entity.Product;
 import victor.training.java8.stream.order.entity.Order.PaymentMethod;
+
+import static java.util.stream.Collectors.*;
 
 @Service
 public class TransformStreams {
@@ -79,7 +74,11 @@ public class TransformStreams {
 	 * Note: Order.getPaymentMethod()
 	 */
 	public Set<PaymentMethod> p02_getUsedPaymentMethods(Customer customer) {
-		return null; 
+		return customer.getOrders().stream()
+//			.map(o -> o.getPaymentMethod())
+			.map(Order::getPaymentMethod)
+			.collect(toSet())
+				;
 	}
 	
 	/**
@@ -87,7 +86,10 @@ public class TransformStreams {
 	 * Note: Order.getCreationDate()
 	 */
 	public SortedSet<LocalDate> p03_getOrderDatesAscending(Customer customer) {
-		return null; 
+		return customer.getOrders().stream()
+			.map(Order::getCreationDate)
+//			.collect(Collectors.toCollection(() -> new TreeSet<>()));
+			.collect(Collectors.toCollection(TreeSet::new));
 	}
 	
 	
@@ -95,14 +97,16 @@ public class TransformStreams {
 	 * @return a map order.id -> order
 	 */
 	public Map<Long, Order> p04_mapOrdersById(Customer customer) {
-		return null; 
+		return customer.getOrders().stream()
+				.collect(toMap(Order::getId, o -> o));
 	}
 	
 	/** 
 	 * Orders grouped by Order.paymentMethod
 	 */
 	public Map<PaymentMethod, List<Order>> p05_getProductsByPaymentMethod(Customer customer) {
-		return null; 
+		return customer.getOrders().stream()
+			.collect(Collectors.groupingBy(Order::getPaymentMethod)); // SQL: GROUP BY
 	}
 	
 	// -------------- MOVIE BREAK :p --------------------
@@ -116,13 +120,18 @@ public class TransformStreams {
 	 */
 	public Map<Product, Long> p06_getProductCount(Customer customer) {
 		
-		List<OrderLine> allLines = new ArrayList<>();
-		
-		for (Order order : customer.getOrders()) {
-			allLines.addAll(order.getOrderLines());
-		}
-		return null; 
-		
+//		List<OrderLine> allLines = new ArrayList<>();
+//
+//		for (Order order : customer.getOrders()) {
+//			allLines.addAll(order.getOrderLines());
+//		}
+
+		// ai un stream de parinti, cum obtii un stream din copiilor joinati intre ei?
+		List<OrderLine> allLines = customer.getOrders().stream()
+			.flatMap(o -> o.getOrderLines().stream()).collect(toList());
+
+		return allLines.stream()
+			.collect(groupingBy(OrderLine::getProduct, summingLong(OrderLine::getCount)));
 	}
 	
 	/**
