4a35a75d520faff44bf3064f50d734460884c0a4
Victor Rentea <victorrentea@gmail.com>
2021-07-15 20:52:51 +0300

    Try Monad : returning the exception like a normal result

--- src/main/java/victor/training/java8/advanced/Exceptions.java
+++ src/main/java/victor/training/java8/advanced/Exceptions.java
@@ -1,10 +1,14 @@
 package victor.training.java8.advanced;
 
+import io.vavr.control.Either;
+import io.vavr.control.Try;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
@@ -15,10 +19,23 @@ public class Exceptions {
    public List<LocalDate> parseDates(List<String> dateStrList) {
       DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-      List<LocalDate> dates = dateStrList.stream()
-          .map(text -> LocalDate.parse(text, pattern))
+      List<Try<LocalDate>> tries = dateStrList.stream()
+          .map(text -> parse(pattern, text))
           .collect(toList());
 
-      return dates;
+      double successRatio = tries.stream().mapToInt(t -> t.isSuccess() ? 1 : 0).average().orElse(0);
+
+      if (successRatio >= 0.5) {
+         return tries.stream().filter(Try::isSuccess).map(Try::get).collect(Collectors.toList());
+      }
+
+      String errors = tries.stream().filter(Try::isFailure).map(Try::getCause).map(Throwable::getMessage).collect(Collectors.joining());
+
+      throw new IllegalArgumentException(errors);
+
+   }
+
+   private Try<LocalDate> parse(DateTimeFormatter pattern, String text) { // never throws, but might reutrn a failed Try
+      return Try.ofCallable(   () -> LocalDate.parse(text, pattern)   ) ;
    }
 }
