8dfcefe561f4505e56a17c032f3a3f40fd78e743
Victor Rentea <victorrentea@gmail.com>
2021-09-20 12:09:51 +0300

    working dar non FP

--- src/main/java/victor/training/java8/advanced/Exceptions.java
+++ src/main/java/victor/training/java8/advanced/Exceptions.java
@@ -3,22 +3,34 @@ package victor.training.java8.advanced;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
 
+// prod
 public class Exceptions {
 
-   // TODO Return parsable dates if >= 50% valid; otherwise throw IllegalArgumentException (Hint: average)
+   // TODO Return parsable dates if >= 50% valid;
+   //  otherwise throw IllegalArgumentException (Hint: average)
    // Run tests!
    public List<LocalDate> parseDates(List<String> dateStrList) {
       DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-      List<LocalDate> dates = dateStrList.stream()
-          .map(text -> LocalDate.parse(text, pattern))
-          .collect(toList());
+      List<LocalDate> validDates = new ArrayList<>();
+      for (String dateStr : dateStrList) {
+         try {
+            validDates.add(LocalDate.parse(dateStr, pattern));
+         } catch (DateTimeParseException e) {
+            // shawarma
+         }
+      }
+      if (dateStrList.size() - validDates.size() > dateStrList.size()/2) {
+         throw new IllegalArgumentException();
+      }
 
-      return dates;
+      return validDates;
    }
 }
