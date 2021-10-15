e1bbac8b1c31ce99114433eb7d8bd0c36e09f25f
Victor Rentea <victorrentea@gmail.com>
2021-10-15 13:59:21 +0300

    incep

--- src/main/java/victor/training/java8/advanced/LoanPattern.java
+++ src/main/java/victor/training/java8/advanced/LoanPattern.java
@@ -1,33 +1,57 @@
 package victor.training.java8.advanced;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import victor.training.java8.advanced.CSVExporter.ExportContentWriter;
-import victor.training.java8.advanced.repo.UserRepo;
+import org.springframework.stereotype.Service;
+import victor.training.java8.advanced.repo.OrderRepo;
+
+//@RefresScope
+@Service
+class OrderExporter {
+   @Autowired
+   private OrderRepo orderRepo;
+   @Value("${export.folder.out}")
+   private File folder;
+
+   public void exportFile() {
+      File file = new File(folder, "orders.csv");
+      long t0 = System.currentTimeMillis();
+      try (Writer writer = new FileWriter(file)) {
+         System.out.println("Starting export to: " + file.getAbsolutePath());
+
+         writer.write("OrderID;Date\n");
+
+//			orderRepo.findByActiveTrue()
+//				.map(o -> o.getId() + ";" + o.getCreationDate())
+//				.forEach(writer::write);
+         System.out.println("File export completed: " + file.getAbsolutePath());
+      } catch (Exception e) {
+         // TODO send email notification
+         throw new RuntimeException("Error exporting data", e);
+      } finally {
+         System.out.println("Export finished in: " + (System.currentTimeMillis()-t0));
+      }
+   }
+}
 
 @RequiredArgsConstructor
 @SpringBootApplication
 public class LoanPattern implements CommandLineRunner {
-   private final CSVExporter csvExporter;
-   private final OrderContentWriter orderContentWriter;
-   private final UserContentWriter userContentWriter;
-
    public static void main(String[] args) {
       SpringApplication.run(LoanPattern.class, args);
    }
 
-   @Autowired
-   private UserRepo userRepo;
-
+   private final OrderExporter orderExporter;
    public void run(String... args) throws Exception {
-
-      csvExporter.exportFile("orders", orderContentWriter::writeContent);
-
-      System.out.println("-------------------");
-      csvExporter.exportFile("users", userContentWriter::writeContent);
+      orderExporter.exportFile();
    }
 }
 
