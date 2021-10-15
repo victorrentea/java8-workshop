dbdbdd78a61399ffbbb32420a608e903e580b0c0
Victor Rentea <victorrentea@gmail.com>
2021-10-15 14:14:05 +0300

    Checkec exception handling cu @SneakyThrows sau Unchecked.consumer <<rock

--- src/main/java/victor/training/java8/advanced/LoanPattern.java
+++ src/main/java/victor/training/java8/advanced/LoanPattern.java
@@ -1,10 +1,8 @@
 package victor.training.java8.advanced;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.Writer;
-
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.jooq.lambda.Unchecked;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -13,6 +11,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
 import victor.training.java8.advanced.repo.OrderRepo;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.function.Consumer;
+
+
+@FunctionalInterface
+ interface ConsumerCareArunca<T> {
+   void accept(T t) throws Exception;
+}
 //@RefresScope
 @Service
 class OrderExporter {
@@ -29,15 +38,36 @@ class OrderExporter {
 
          writer.write("OrderID;Date\n");
 
-//			orderRepo.findByActiveTrue()
-//				.map(o -> o.getId() + ";" + o.getCreationDate())
-//				.forEach(writer::write);
+         orderRepo.findByActiveTrue()
+             .map(o -> o.getId() + ";" + o.getCreationDate() + "\n")
+             .forEach(Unchecked.consumer(writer::write));
+//             .forEach(imbraca(writer::write));
+
          System.out.println("File export completed: " + file.getAbsolutePath());
       } catch (Exception e) {
          // TODO send email notification
          throw new RuntimeException("Error exporting data", e);
       } finally {
-         System.out.println("Export finished in: " + (System.currentTimeMillis()-t0));
+         System.out.println("Export finished in: " + (System.currentTimeMillis() - t0));
+      }
+   }
+
+   public static <T> Consumer<T> imbraca(ConsumerCareArunca<T> consumerOrig) {
+      // astfel de functii produc panica si dezordine in randurile devilor
+      return s -> {
+         try {
+            consumerOrig.accept(s);
+         } catch (Exception e) {
+            throw new RuntimeException(e);
+         }
+      };
+   }
+
+   private void writeSafely(Writer writer, String s) {
+      try {
+         writer.write(s);
+      } catch (IOException e) {
+         throw new RuntimeException(e);
       }
    }
 }
@@ -50,6 +80,7 @@ public class LoanPattern implements CommandLineRunner {
    }
 
    private final OrderExporter orderExporter;
+
    public void run(String... args) throws Exception {
       orderExporter.exportFile();
    }
