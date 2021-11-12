82413f29dc563d7a7ad2574d102f36bc6d351285
Victor Rentea <victorrentea@gmail.com>
2021-11-12 13:12:20 +0200

    BIFunctions on enums

--- src/main/java/victor/training/java8/advanced/FunctionsOnEnums.java
+++ src/main/java/victor/training/java8/advanced/FunctionsOnEnums.java
@@ -2,60 +2,78 @@ package victor.training.java8.advanced;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import victor.training.java8.advanced.repo.OrderRepo;
 
+import java.util.Map;
+import java.util.function.BiFunction;
+
 enum MovieType {
-   REGULAR {
-      @Override
-      public int computePrice(int daysRented) {
-         return daysRented + 1;
-      }
-   },
-   NEW_RELEASE {
-      @Override
-      public int computePrice(int daysRented) {
-         return daysRented * 2; // PROBLEM: LOGIC in enums ====> SHOCK for the first BUG in an enum
-      }
-   },
-   CHILDREN {
-      @Override
-      public int computePrice(int daysRented) {
-//         orderRepo.findByActiveTrue() // PROBLEM: donest compile
-         return 5;
-      }
+   REGULAR(FunctionsOnEnums::getRegularPrice),
+   NEW_RELEASE(FunctionsOnEnums::getNewReleasePrice),
+   CHILDREN(FunctionsOnEnums::getChildrenPrice);
+
+   public final BiFunction<FunctionsOnEnums, Integer, Integer> priceLogic;
+
+   MovieType(BiFunction<FunctionsOnEnums, Integer, Integer> priceLogic) {
+      this.priceLogic = priceLogic;
    }
-//   ,ELDERS // doesn't compike anymore
-   ;
-
-
-   public abstract int computePrice(int daysRented);
-
 }
 
+
+@Service
 public class FunctionsOnEnums {
+
+   public int computePrice(MovieType type, int daysRented) {
+//      switch (type) {
+//         case REGULAR:
+//            return getRegularPrice(daysRented);
+//         case NEW_RELEASE:
+//            return getNewReleasePrice(daysRented);
+//         case CHILDREN:
+//            return getChildrenPrice(daysRented);
+//         default:
+//            throw new NoMatchingCase();
+//      }
+
+//      return map.get(type).apply(this, daysRented); // WORKS !
+
+      return type.priceLogic.apply(this, daysRented);
+   }
+
+   // we are trying to reference an instance method FROM A STATIC CONTEXT (static map)
+   // => you need to TAKE THE INSTANCE of the holder type as the first parameter
+
+   // consider making this instance !
+   public static final Map<MovieType, BiFunction<FunctionsOnEnums, Integer, Integer>> map = Map.of(
+//       MovieType.CHILDREN, (self, daysRented) -> self.getChildrenPrice(),
+//       MovieType.REGULAR, (self, daysRented) -> self.getRegularPrice(daysRented) ///
+       MovieType.CHILDREN, FunctionsOnEnums::getChildrenPrice,
+       MovieType.REGULAR, FunctionsOnEnums::getRegularPrice,
+       MovieType.NEW_RELEASE, FunctionsOnEnums::getNewReleasePrice
+   );
+
+   public int getRegularPrice(int daysRented) {
+      return daysRented + 1;
+   }
+
+   public int getNewReleasePrice(int daysRented) {
+      return daysRented * 2;
+   }
    @Autowired
    OrderRepo orderRepo; // unreferenceable from anything in enum
+   public int getChildrenPrice(int daysRented) {
+      return (int) orderRepo.count();
+   }
 
    public static void main(String[] args) {
 
-      System.out.println(computePrice(MovieType.REGULAR, 2));
-      System.out.println(computePrice(MovieType.NEW_RELEASE, 2));
-      System.out.println(computePrice(MovieType.CHILDREN, 2));
+      System.out.println(new FunctionsOnEnums().computePrice(MovieType.REGULAR, 2));
+      System.out.println(new FunctionsOnEnums().computePrice(MovieType.NEW_RELEASE, 2));
+      System.out.println(new FunctionsOnEnums().computePrice(MovieType.CHILDREN, 2));
    }
 
    // we handle kafka messages based on some code -> enum type, JMS, records from file
-   public static int computePrice(MovieType type, int daysRented) {
-      switch (type) {
-         case REGULAR:
-            return daysRented + 1;
-         case NEW_RELEASE:
-            return daysRented * 2;
-         case CHILDREN:
-            return 5;
-         default:
-            throw new NoMatchingCase();
-      }
-   }
 
    public static int getMaxRentedDays(MovieType type) {
       switch (type) {
--- src/test/java/victor/training/java8/advanced/FunctionsOnEnumsTest.java
+++ src/test/java/victor/training/java8/advanced/FunctionsOnEnumsTest.java
@@ -1,5 +1,6 @@
 package victor.training.java8.advanced;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,12 +12,18 @@ class FunctionsOnEnumsTest {
    void computePriceTest() {
       for (MovieType value : MovieType.values()) {
          try {
-            FunctionsOnEnums.computePrice(value, -1);
+            new FunctionsOnEnums().computePrice(value, -1);
          } catch (Exception e) {
             if (e instanceof NoMatchingCase) {
                fail();
             }
          }
+      }
+   }
+   @Test
+   void computePriceTest2() {
+      for (MovieType value : MovieType.values()) {
+         Assertions.assertThat(FunctionsOnEnums.map.get(value)).isNotNull();
       }
    }
 }
