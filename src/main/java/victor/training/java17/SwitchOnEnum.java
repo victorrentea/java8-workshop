060a04c3ca9ed84ba0981abc7c484d7f1e460b3e
Victor Rentea <victorrentea@gmail.com>
2021-11-12 13:48:45 +0200

    theo

--- src/main/java/victor/training/java17/SwitchOnEnum.java
+++ src/main/java/victor/training/java17/SwitchOnEnum.java
@@ -6,9 +6,15 @@ import java.util.function.BiFunction;
 
 public class SwitchOnEnum {
 
+   private int someCounter = 100;
+
    public int computePrice(MovieType type, int daysRented) {
+
       int vint = switch (daysRented) {
-         case 1 -> daysRented + 1;
+         case 1 -> {
+            if (someCounter < 1000) yield 2;
+            yield daysRented + 1;
+         }
          case 2 -> daysRented * 2;
          case 3 -> 5;
          default -> throw new IllegalStateException("mandatory if switch on NON-enum and switch used as an expression");
@@ -16,11 +22,12 @@ public class SwitchOnEnum {
       int v = switch (type) {
          case REGULAR -> daysRented + 1;
          case NEW_RELEASE -> daysRented * 2;
-         case CHILDREN -> 5;
+         default -> 5;
 //         default -> // if you switch on enum the compiler allows you to skip the default,
 //         even if your switch is used as an expression
       };
-      //         default ->
+
+      ///Regardless of what you put after the arrow ->,
       int vv;
       switch (type) {
          case REGULAR:
@@ -64,5 +71,5 @@ enum MovieType {
    REGULAR,
    NEW_RELEASE,
    CHILDREN,
-//   ELDERS
+   ELDERS
 }
