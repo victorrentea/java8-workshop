d6c148d8ad2c38274678be49e680d59a498173e8
Victor Rentea <victorrentea@gmail.com>
2021-11-12 14:08:05 +0200

    add text blocks

---
+++ src/main/java/victor/training/java/language/TextBlocks.java
@@ -0,0 +1,38 @@
+package victor.training.java.language;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.test.web.servlet.MockMvc;
+import victor.training.java.stream.order.entity.Order;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class TextBlocks {
+
+   interface SomeSpringDataRepo extends JpaRepository<Order, Long> {
+      @Query("SELECT t FROM TeachingActivity a JOIN a.teachers t WHERE "
+         + "a.id IN (SELECT c.id FROM StudentsYear y JOIN y.courses c WHERE y.id = :yearId) "
+         + "OR a.id IN (SELECT lab.id FROM StudentsYear y JOIN y.groups g JOIN g.labs lab WHERE y.id = :yearId)")
+      List<Order> complexQuery(long id1, long id2); // bogus
+   }
+
+   @Autowired
+   MockMvc mockMvc;
+   @Test
+   void test() {
+      mockMvc.perform(post("/product/search")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content("{\"name\":\"somth\"}") // add one more criteria
+          )
+          .andExpect(status().isOk()) // 200
+          .andExpect(jsonPath("$", hasSize(1)));
+   }
+
+   // TODO + test of Loan Pattern ->
+}
