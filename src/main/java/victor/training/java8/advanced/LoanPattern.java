9bc35e9cf72cf094db927b4a0f8e5d6301a42f86
Victor Rentea <victorrentea@gmail.com>
2021-11-12 11:36:06 +0200

    Loan Pattern DONE

--- src/main/java/victor/training/java8/advanced/LoanPattern.java
+++ src/main/java/victor/training/java8/advanced/LoanPattern.java
@@ -1,11 +1,5 @@
 package victor.training.java8.advanced;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
-import java.util.function.Consumer;
-
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.jooq.lambda.Unchecked;
@@ -15,61 +9,74 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
-import org.springframework.test.annotation.Timed;
+import org.springframework.transaction.annotation.Transactional;
+import victor.training.java8.advanced.FileExporter.FileContentWriter;
 import victor.training.java8.advanced.repo.OrderRepo;
+import victor.training.java8.advanced.repo.UserRepo;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.function.Consumer;
 
 interface MyConsumer<T> {
    void accept(T t) throws Exception;
 }
+
 @Service
-class OrderExporter {
-   @Autowired
-   private OrderRepo orderRepo;
+class FileExporter {
+
    @Value("${export.folder.out}")
    private File folder;
 
-//   @Timed(micrometer)
-   public void exportFile() {
-      File file = new File(folder, "orders.csv");
+   @FunctionalInterface
+   interface FileContentWriter {
+      void write(Writer writer) throws Exception;
+   }
+
+   //   @Timed(micrometer)
+   @Transactional
+   public void exportFile(String fileName, FileContentWriter contentWriter) {
+      File file = new File(folder, fileName);
       long t0 = System.currentTimeMillis();
       try (Writer writer = new FileWriter(file)) { // ~ anaology to any resource that needs cleaning / releasing : sockets, jms connections, jdbc conn
          System.out.println("Starting export to: " + file.getAbsolutePath());
 
-         writer.write("OrderID;Date\n");
-
-
-         orderRepo.findByActiveTrue()
-             .map(o -> o.getId() + ";" + o.getCreationDate())
-//				.forEach(conversion(writer::write));
-             .forEach(Unchecked.consumer(writer::write));
+         contentWriter.write(writer);
 
          System.out.println("File export completed: " + file.getAbsolutePath());
       } catch (Exception e) {
          // TODO send email notification
          throw new RuntimeException("Error exporting data", e);
       } finally {
-         System.out.println("Export finished in: " + (System.currentTimeMillis()-t0));
+         System.out.println("Export finished in: " + (System.currentTimeMillis() - t0));
       }
    }
 
-   // UTIL wornderful idea
-   public static <T> Consumer<T> conversion(MyConsumer<T> f) {
-      return input -> {
-         try {
-            f.accept(input);
-         } catch (Exception e) {
-            throw new RuntimeException(e);
-         }
-      };
+}
+
+@Service
+class ExportFormats {
+
+   @Autowired
+   private OrderRepo orderRepo;
+
+   public void writeOrdersContent(Writer writer) throws IOException {
+      writer.write("OrderID;Date\n");
+      orderRepo.findByActiveTrue()
+          .map(o -> o.getId() + ";" + o.getCreationDate() + "\n")
+          .forEach(Unchecked.consumer(writer::write));
    }
 
-   //   @SneakyThrows // maybe NOT
-   private void javaSucks(Writer writer, String str) {
-      try {
-         writer.write(str);
-      } catch (IOException e) {
-         throw new RuntimeException(e);
-      }
+   @Autowired
+   UserRepo userRepo;
+
+   public void writeUserContent(Writer writer) throws IOException {
+      writer.write("username;fullname\n");
+      userRepo.findAll().stream()
+          .map(u -> u.getUsername() + ";" + u.getFullName() + "\n")
+          .forEach(Unchecked.consumer(writer::write));
    }
 }
 
@@ -80,10 +87,43 @@ public class LoanPattern implements CommandLineRunner {
       SpringApplication.run(LoanPattern.class, args);
    }
 
-   private final OrderExporter orderExporter;
+   private final FileExporter fileExporter;
+   private final ExportFormats exportFormats;
+
    public void run(String... args) throws Exception {
-      orderExporter.exportFile();
+      fileExporter.exportFile("orders.csv", exportFormats::writeOrdersContent);
+
+      fileExporter.exportFile("users.csv", exportFormats::writeUserContent);
+
+
+//      fileExporter.exportFile("file.name", new FileContentWriter() {
+//         @Override
+//         public void write(Writer writer) throws Exception {
+//
+//         }
+//      });
 
    }
-}
 
+
+   // UTIL wornderful idea
+//   public static <T> Consumer<T> conversion(MyConsumer<T> f) {
+//      return input -> {
+//         try {
+//            f.accept(input);
+//         } catch (Exception e) {
+//            throw new RuntimeException(e);
+//         }
+//      };
+//   }
+
+//
+//   //   @SneakyThrows // maybe NOT
+//   private void javaSucks(Writer writer, String str) {
+//      try {
+//         writer.write(str);
+//      } catch (IOException e) {
+//         throw new RuntimeException(e);
+//      }
+//   }
+}
---
+++ src/test/java/victor/training/java8/advanced/ExportFormatsTest.java
@@ -0,0 +1,40 @@
+package victor.training.java8.advanced;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import victor.training.java8.advanced.model.Order;
+import victor.training.java8.advanced.repo.OrderRepo;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.rmi.MarshalException;
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class ExportFormatsTest {
+@Mock
+   OrderRepo orderRepo;
+@InjectMocks ExportFormats exportFormats;
+   @Test
+   void writeOrdersContent() throws IOException {
+      Mockito.when(orderRepo.findByActiveTrue()).thenReturn(Stream.of(new Order().setId(1L).setCreationDate(LocalDate.parse("2021-11-12"))));
+
+      Writer writer = new StringWriter();
+      exportFormats.writeOrdersContent(writer);
+
+      String actual = writer.toString();
+
+      Assertions.assertThat(actual).isEqualTo("OrderID;Date\n" +
+                                              "1;2021-11-12\n");
+   }
+}
