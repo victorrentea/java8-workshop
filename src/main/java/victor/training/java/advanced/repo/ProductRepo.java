5e47564b159d3c67af99d7b3c6ae817ee3a70a63
Victor Rentea <victorrentea@gmail.com>
2021-11-15 13:41:23 +0200

    use custom JPA repo

--- src/main/java/victor/training/java/advanced/repo/ProductRepo.java
+++ src/main/java/victor/training/java/advanced/repo/ProductRepo.java
@@ -2,11 +2,12 @@ package victor.training.java.advanced.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import victor.training.java.advanced.model.Product;
+import victor.training.java.advanced.repo.custom.CustomJpaRepository;
 
 import java.util.List;
 import java.util.stream.Stream;
 
-public interface ProductRepo extends JpaRepository<Product, Long> {
+public interface ProductRepo extends CustomJpaRepository<Product, Long> {
    List<Product> findByHiddenTrue();
 
    Product findByNameContaining(String namePart);
