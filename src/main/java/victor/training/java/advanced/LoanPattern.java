9af988ccb2126e6a0a298080c38c254b296917e3
Victor Rentea <victorrentea@gmail.com>
2022-02-25 11:40:39 +0200

    Interfete functionale custom

--- src/main/java/victor/training/java/advanced/LoanPattern.java
+++ src/main/java/victor/training/java/advanced/LoanPattern.java
@@ -8,11 +8,27 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Service;
 import victor.training.java.advanced.repo.OrderRepo;
+import victor.training.java.advanced.repo.UserRepo;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Writer;
 import java.util.function.Consumer;
+//   @FunctionalInterface
+//   public interface ConsumerChecked<T> {
+//      void accept(T t) throws Exception;
+//   }
+   // 2nd level higher order function : primeste si intoarce fct: evita sa scrii TU asa ceva: greu debug
+//   public static <T>  Consumer<T> wrapChecked(ConsumerChecked<T> consumerChecked) {
+//      return s -> {
+//         try {
+//            consumerChecked.accept(s);
+//         } catch (Exception e) {
+//            throw new RuntimeException(e);
+//         }
+//      };
+//   }
 
 @Service
 class FileExporter {
@@ -22,20 +38,16 @@ class FileExporter {
    private File folder;
 
    @FunctionalInterface
-   public interface ConsumerChecked<T> {
-      void accept(T t) throws Exception;
+   interface ExportFileWriter {
+      void writeContent(Writer writer) throws IOException;
    }
-   public void exportFile() {
-      File file = new File(folder, "orders.csv");
+   public void exportFile(String fileName, ExportFileWriter contentWriter) {
+      File file = new File(folder, fileName);
       long t0 = System.currentTimeMillis();
       try (Writer writer = new FileWriter(file)) {
          System.out.println("Starting export to: " + file.getAbsolutePath());
 
-         writer.write("OrderID;Date\n");
-
-         orderRepo.findByActiveTrue()
-             .map(order -> order.getId() + ";" + order.getCreationDate() + "\n")
-             .forEach(Unchecked.consumer(writer::write));
+         contentWriter.writeContent(writer);
 
          System.out.println("File export completed: " + file.getAbsolutePath());
       } catch (Exception e) {
@@ -46,16 +58,23 @@ class FileExporter {
       }
    }
 
-   // 2nd level higher order function : primeste si intoarce fct: evita sa scrii TU asa ceva: greu debug
-   public static <T>  Consumer<T> wrapChecked(ConsumerChecked<T> consumerChecked) {
-      return s -> {
-         try {
-            consumerChecked.accept(s);
-         } catch (Exception e) {
-            throw new RuntimeException(e);
-         }
-      };
+   public void writeContent(Writer writer) throws IOException {
+      writer.write("OrderID;Date\n");
+      orderRepo.findByActiveTrue()
+          .map(order -> order.getId() + ";" + order.getCreationDate() + "\n")
+          .forEach(Unchecked.consumer(writer::write));
    }
+
+   @Autowired
+   private UserRepo userRepo;
+
+   public void writeUserContent(Writer writer) throws IOException {
+      writer.write("username;fullname\n");
+      userRepo.findAll().stream()
+          .map(user -> user.getUsername() + ";" + user.getFullName() + "\n")
+          .forEach(Unchecked.consumer(writer::write));
+   }
+
 
 }
 
@@ -69,8 +88,9 @@ public class LoanPattern implements CommandLineRunner {
    private final FileExporter fileExporter;
 
    public void run(String... args) throws Exception {
-      fileExporter.exportFile();
+      fileExporter.exportFile("orders.csv", fileExporter::writeContent);
 
+      fileExporter.exportFile("users.csv", fileExporter::writeUserContent);
       // TODO implement export of users too
    }
 }
