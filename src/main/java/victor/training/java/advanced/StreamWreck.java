21659dea67fb9da8b9c590398f190ac7f049413a
victorrentea <victorrentea@gmail.com>
2022-05-11 16:08:29 +0300

    stream wreck

--- src/main/java/victor/training/java/advanced/StreamWreck.java
+++ src/main/java/victor/training/java/advanced/StreamWreck.java
@@ -1,32 +1,41 @@
 package victor.training.java.advanced;
 
+import lombok.extern.slf4j.Slf4j;
+import org.mockito.Mockito;
 import victor.training.java.advanced.model.Order;
 import victor.training.java.advanced.model.OrderLine;
 import victor.training.java.advanced.model.Product;
 import victor.training.java.advanced.repo.ProductRepo;
 
+import javax.swing.*;
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
+import java.util.stream.Stream;
 
 import static java.util.function.Predicate.not;
 import static java.util.stream.Collectors.*;
 
+@Slf4j
 public class StreamWreck {
-   private ProductRepo productRepo;
+   private ProductRepo productRepo = Mockito.mock(ProductRepo.class);
 
+   public static void main(String[] args) {
+      new StreamWreck().getFrequentOrderedProducts(Collections.emptyList());
+   }
    public List<Product> getFrequentOrderedProducts(List<Order> orders) {
       Set<Long> hiddenProductIds = new HashSet<>(productRepo.getHiddenProductIds());
       Map<Product, Integer> recentBoughtProducts = countRecentProducts(orders);
       List<Product> frequentRecentProducts = recentBoughtProducts.entrySet().stream()
               .filter(e -> e.getValue() >= 10)
               .map(Entry::getKey)
-              .collect(toList());
+              .collect(toList())
+              ;
 
-      // CR: if  many frequen
+      // CR: if  many frequent products > raise alert
+//      if (frequentRecentProducts.count() > 1000) {
+//         log.warn("VALEU!");
+//      }
 
       return frequentRecentProducts.stream()
 //				.filter(p -> !p.isDeleted())
