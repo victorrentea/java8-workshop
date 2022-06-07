77fd45e921df41a230f7113c199ca33ef2a01dc3
victorrentea <victorrentea@gmail.com>
2022-06-07 23:54:13 +0300

    BiFunctions on enums as an alternative to polyporphism

--- src/main/java/victor/training/java/advanced/Switch.java
+++ src/main/java/victor/training/java/advanced/Switch.java
@@ -4,50 +4,55 @@ package victor.training.java.advanced;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 enum MovieType {
-   REGULAR{
-      @Override
-      public int computePrice(int days) {
-         return days + 1;
-      }
-   },
-   NEW_RELEASE {
-      @Override
-      public int computePrice(int days) {
-         return days * 2;
-      }
-   },
-   CHILDREN {
-      @Override
-      public int computePrice(int days) {
-         return 5;
-      }
-   },
-//ELDERS
-   ;public abstract int computePrice(int days);
+   REGULAR(Switch::regularPrice),
+   NEW_RELEASE(Switch::newReleasePrice),
+   CHILDREN(Switch::childrenPrice),
+   ELDERS((a, days) -> 1)
+   ;
+
+   public final BiFunction<Switch, Integer, Integer> priceAlgo;
+
+   MovieType(BiFunction<Switch, Integer,Integer> priceAlgo) {
+      this.priceAlgo = priceAlgo;
+   }
 }
 
 @Service
 public class Switch {
    @Value("${children.price}")
-   private int childrenPrice = 5; // pretend Spring is ON
+   private int childrenPrice; // pretend Spring is ON
 
    // @see tests
-   public static int computePrice(MovieType type, int days) {
-      return type.computePrice(days);
+   public int computePrice(MovieType type, int days) {
+      return type.priceAlgo.apply(this, days);
 //      switch (type) {
 //         case REGULAR:
-//            return days + 1;
+//            return regularPrice(days);
 //         case NEW_RELEASE:
-//            return days * 2;
+//            return newReleasePrice(days);
 //         case CHILDREN:
-//            return 5;
-//
+//            return childrenPrice(days);
 //         default:
 //            throw new IllegalStateException("Unexpected value: " + type);
 //      }
-//      return 0; // opinions?
    }
+
+   public int childrenPrice(int days) {
+      return 5;
+   }
+
+   public int newReleasePrice(int days) {
+      return days * 2;
+   }
+
+   public int regularPrice(int days) {
+      return days + 1;
+   }
+
 
    public void auditDelayReturn(MovieType movieType, int delayDays) {
       switch (movieType) {
