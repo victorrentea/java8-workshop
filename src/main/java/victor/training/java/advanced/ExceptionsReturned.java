6bc6dd5e3a94a4e60104d9305796ba7b1a7f247d
victorrentea <victorrentea@gmail.com>
2022-08-02 22:02:50 +0300

    Optional basics

--- src/main/java/victor/training/java/advanced/ExceptionsReturned.java
+++ src/main/java/victor/training/java/advanced/ExceptionsReturned.java
@@ -1,43 +1,55 @@
 package victor.training.java.advanced;
 
+import io.vavr.control.Try;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 import static java.util.function.Predicate.not;
 import static java.util.stream.Collectors.toList;
 
 public class ExceptionsReturned {
 
-   // ** Requirements **
-   // Some dates might have invalid format
-   // - if >= 50% valid => return the parseable dates,
-   // - else => throw IllegalArgumentException
-   // I WANT TO SEE ALL THE FAILED DATES>
-   /* @see tests */
-   public List<LocalDate> parseDates(List<String> dateStrList) {
-      DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    // ** Requirements **
+    // Some dates might have invalid format
+    // - if >= 50% valid => return the parseable dates,
+    // - else => throw IllegalArgumentException
+    // I WANT TO SEE ALL THE FAILED DATES>
+    /* @see tests */
+    public List<LocalDate> parseDates(List<String> dateStrList) {
+        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-      List<String> failedDates = new ArrayList<>();
-      List<LocalDate>dates = dateStrList.stream()
-          .map(text -> {
-             try {
-                return LocalDate.parse(text, pattern);
-             } catch (Exception e) {
-                failedDates.add(text);
-                return null;
-             }
-          })
-           .filter(not(Objects::isNull))
-          .collect(toList());
+        List<String> failedDates = new ArrayList<>();
+        List<Try<LocalDate>> tries = dateStrList.stream()
+                .map(text -> Try.of(() -> LocalDate.parse(text, pattern)))
+                .toList();
 
+        long successNumber = tries.stream().filter(Try::isSuccess).count();
 
-      if (dates.size() > dateStrList.size() / 2) {
-         return dates;
-      } else {
-         throw new IllegalArgumentException();
-      }
-   }
+        CompletableFuture<String> futureValue = CompletableFuture.supplyAsync(() -> networkCall());
+        futureValue = CompletableFuture.completedFuture("aaa");
+        futureValue = CompletableFuture.failedFuture(new IllegalArgumentException());
+
+//        Mono<String> m = Mono.erorr(); Mono.just
+
+        if (successNumber > dateStrList.size() / 2) {
+            return tries.stream().filter(Try::isSuccess).map(Try::get).collect(toList());
+        } else {
+            String allErrors = tries.stream()
+                            .filter(Try::isFailure)
+                            .map(Try::getCause)
+                            .map(Throwable::getMessage)
+                            .collect(Collectors.joining("\n"));
+            throw new IllegalArgumentException(allErrors);
+        }
+    }
+
+    private static String networkCall() {
+        return "data";
+    }
 }
--- src/main/java/victor/training/java/advanced/Optionals.java
+++ src/main/java/victor/training/java/advanced/Optionals.java
@@ -6,31 +6,42 @@ import victor.training.java.advanced.model.MemberCard;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @SuppressWarnings("ConstantConditions")
 public class Optionals {
-	public static void main(String[] args) {
-		// test: 60, 10, no MemberCard
-		System.out.println(getDiscountLine(new Customer(new MemberCard(60))));
-	}
+    public static void main(String[] args) {
+        // test: 60, 10, no MemberCard
+        System.out.println(getDiscountLine(new Customer(new MemberCard(60))));
+        System.out.println(getDiscountLine(new Customer(new MemberCard(10))));
+        System.out.println(getDiscountLine(new Customer(null)));
+    }
 
-	public static String getDiscountLine(Customer customer) {
-		return "Discount: " + getApplicableDiscountPercentage(customer.getMemberCard()).getGlobalPercentage();
-	}
+    public static String getDiscountLine(Customer customer) {
+        // to get rid of Stream<Stream<   Optioan<Optional<   Mono<Mono<
+        return customer.getMemberCard()
+                .flatMap(Optionals::getApplicableDiscountPercentage)
+                .map(Discount::getGlobalPercentage)
+                .map(p -> "Discount%: " + p)
+                .orElse("");
+    }
 
-	private static Discount getApplicableDiscountPercentage(MemberCard card) {
-		if (card.getFidelityPoints() >= 100) {
-			return new Discount(5);
-		}
-		if (card.getFidelityPoints() >= 50) {
-			return new Discount(3);
-		}
-		return null;
-	}
+    private static Optional<Discount> getApplicableDiscountPercentage(MemberCard card) {
+        if (card == null) {
+            return Optional.empty();
+        }
+        if (card.getFidelityPoints() >= 100) {
+            return Optional.of(new Discount(5));
+        }
+        if (card.getFidelityPoints() >= 50) {
+            return Optional.of(new Discount(3));
+        }
+        return Optional.empty();
+    }
 }
 
 @Data
 class Discount {
-	private final int globalPercentage;
-	private Map<String, Integer> categoryDiscounts = new HashMap<>();
+    private final int globalPercentage;
+    private Map<String, Integer> categoryDiscounts = new HashMap<>();
 }
