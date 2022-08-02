ef98be68f7a2cf92a81a273e16ba9510ae76f5de
victorrentea <victorrentea@gmail.com>
2022-08-03 00:22:40 +0300

    END

--- src/main/java/victor/training/java/advanced/ParallelStreams.java
+++ src/main/java/victor/training/java/advanced/ParallelStreams.java
@@ -4,7 +4,10 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
@@ -12,24 +15,31 @@ import static victor.training.java.advanced.tricks.ConcurrencyUtil.sleepq;
 
 @Slf4j
 public class ParallelStreams {
-   public static void main(String[] args) {
+   public static void main(String[] args) throws ExecutionException, InterruptedException {
 //      Enemy.parallelRequest();
 
       long t0 = System.currentTimeMillis();
 
-      List<Integer> list = IntStream.range(1,100).boxed().collect(toList());
+      List<Integer> list = IntStream.range(1, 100).boxed().toList();
 
-      List<Integer> result = list.stream()
-          .filter(i -> {
-             log.debug("Filter " + i);
-             return i % 2 == 0;
-          })
-          .map(i -> {
-             log.debug("Map " + i);
-             sleepq(100); // do some 'paralellizable' I/O work (DB, REST, SOAP)
-             return i * 2;
-          })
-          .collect(toList());
+      Stream<Integer> stream = list.parallelStream()
+              .filter(i -> {
+                 log.debug("Filter " + i);
+                 return i % 2 == 0;
+              })
+              .map(i -> {
+                 log.debug("Map " + i);
+                 sleepq(100); // what exactly takes this time?
+                 // NETWORKING: SELECT, GET, POST, WSDL call, RMI < 95% of cases your are here
+                 // HEAVY CPU: generatign PDF, rendering a chart, video compression, asymmetric signatures verification
+
+                 // do some 'paralellizable' I/O work (DB, REST, SOAP)
+                 return i * 2;
+              });
+
+      ForkJoinPool myPool = new ForkJoinPool(3); // Evil syntax
+
+      List<Integer> result =   myPool.submit(  () -> stream.collect(toList()) ).get();
       log.debug("Got result: " + result);
 
       long t1 = System.currentTimeMillis();
