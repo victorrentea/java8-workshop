f2819c781f6704b0ce56ce6ebff2933b432178d7
Victor Rentea <victorrentea@gmail.com>
2023-02-17 14:49:17 +0200

    default throw impotriva VALORI noi in enum

--- src/main/java/victor/training/java/advanced/Switch.java
+++ src/main/java/victor/training/java/advanced/Switch.java
@@ -8,16 +8,16 @@ enum MovieType {
    REGULAR,
    NEW_RELEASE,
    CHILDREN,
-
+   BABACI,
 }
 
 @Service
 public class Switch {
    @Value("${children.price}")
    private int childrenPrice = 5; // pretend Spring is ON
-
    // @see tests
    public static int computePrice(MovieType type, int days) {
+      // PROBLEMA switchului este ca atunci cand adaugi un tip nou de film, e posibil sa uiti sa adaugi case: necesar aici
       switch (type) {
          case REGULAR:
             return days + 1;
@@ -25,8 +25,9 @@ public class Switch {
             return days * 2;
          case CHILDREN:
             return 5;
+         default:
+            throw new IllegalStateException("Unexpected value: " + type);
       }
-      return 0; // opinions?
    }
 
    public void auditDelayReturn(MovieType movieType, int delayDays) {
