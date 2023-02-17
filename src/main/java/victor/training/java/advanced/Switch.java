e7e6b523bbf28c9ba4a39b8df9bb3b1795071c3a
Victor Rentea <victorrentea@gmail.com>
2023-02-17 15:09:03 +0200

    BiFunctions pe Enum vs Switch

--- src/main/java/victor/training/java/advanced/Switch.java
+++ src/main/java/victor/training/java/advanced/Switch.java
@@ -1,49 +1,72 @@
 package victor.training.java.advanced;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.function.BiFunction;
+
 enum MovieType {
-   REGULAR,
-   NEW_RELEASE,
-   CHILDREN
+   REGULAR(Switch::computeRegularPrice),
+   NEW_RELEASE(Switch::computeNewReleasePrice),
+   CHILDREN(Switch::computeChildrenPrice),
+   BABACI((s,d)->7)
 //  {
 //      @Override
 //      public int computePrice(int days) {
 //         return Switch.childrenPrice; // 1) nu compileaza; 2) frate... logica in enum!??!! really ?!
 //      }
 //   }
-   ,
 //   BABACI,
-   ;
-
+//   ;
    // Solutia #1
 //   public abstract int computePrice(int days); // solutia 1 OMG
-
    // Solutia #2 FP maniaca
+;
+   // Solutia #2: 'pointer' din Enum la functia din @Service
+   // -bidirectional coupling
+   // -BiFunction ??!!🤢 un pic prea geek parca
+   // -1 param doar la functie ca sa folosesti standard Function Interfaces din java 8
+   // +ok ca te obliga la compilare sa dai behavior
+   public final BiFunction<Switch, Integer, Integer> priceFunction;
 
+   MovieType(BiFunction<Switch, Integer, Integer> priceFunction) {
+      this.priceFunction = priceFunction;
+   }
 }
 
 @Service
 public class Switch {
    @Value("${children.price}")
    public int childrenPrice = 5; // pretend Spring is ON
-   // @see tests
-   public static int computePrice(MovieType type, int days) {
+   public int computePrice(MovieType type, int days) {
+      return type.priceFunction.apply(this,days);
       // PROBLEMA switchului este ca atunci cand adaugi un tip nou de film, e posibil sa uiti sa adaugi case: necesar aici
-      switch (type) {
-         case REGULAR:
-            return days + 1;
-         case NEW_RELEASE:
-            return days * 2;
-         case CHILDREN:
-            return 5;
-         default:
-            throw new IllegalStateException("Unexpected value: " + type);
-      }
+      //      switch (type) {
+      //         case REGULAR:
+      //            return computeRegularPrice(days);
+      //         case NEW_RELEASE:
+      //            return computeNewReleasePrice(days);
+      //         case CHILDREN:
+      //            return computeChildrenPrice(days);
+      //         default:
+      //            throw new IllegalStateException("Unexpected value: " + type);
+      //      }
    }
+   public int computeRegularPrice(int days) {
+      return days + 1;
+   }
+   // @see tests
+
+   public int computeChildrenPrice(int days) {
+      return childrenPrice;
+   }
+
+   public int computeNewReleasePrice(int days) {
+      return days * 2;
+   }
+
+
 
    public void auditDelayReturn(MovieType movieType, int delayDays) {
       switch (movieType) {
