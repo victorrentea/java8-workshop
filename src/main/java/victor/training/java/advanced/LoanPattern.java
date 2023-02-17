8158072acb2917793d9966479f7eeb95e79e1d6a
Victor Rentea <victorrentea@gmail.com>
2023-02-17 14:38:11 +0200

    Loan pattern: extragi bucata de logica variabila si-o iei ca functie parametru.

--- src/main/java/victor/training/java/advanced/LoanPattern.java
+++ src/main/java/victor/training/java/advanced/LoanPattern.java
@@ -2,12 +2,13 @@ package victor.training.java.advanced;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Writer;
 import java.util.function.Consumer;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.jooq.lambda.Unchecked;
-import org.jooq.lambda.tuple.Tuple2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -15,6 +16,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import victor.training.java.advanced.repo.OrderRepo;
+import victor.training.java.advanced.repo.UserRepo;
 
 import javax.persistence.EntityManager;
 
@@ -25,39 +27,51 @@ interface ConsumeruMeu<T> {
 }
 @Service
 class FileExporter {
-   @Autowired
-   private OrderRepo orderRepo;
+
    @Value("${export.folder.out}")
    private File folder;
    EntityManager em;
 
    @Transactional // necesar pentru a-i spune lui spring sa aloce o connex JDBC pentru intreaga durata
    // a acestei metode, pentru ca REsultSet sa poata fi traversat
-   public void exportFile() {
-      File file = new File(folder, "orders.csv");
+   public void exportFile(String fileName, Consumer<Writer> contentWriteFunction) {
+      File file = new File(folder, fileName);
       long t0 = System.currentTimeMillis();
       try (Writer writer = new FileWriter(file)) {
          System.out.println("Starting export to: " + file.getAbsolutePath());
-
-         writer.write("OrderID;Date\n");
-
-         orderRepo.findByActiveTrue()
-//              .peek(entity -> em.detach(entity)) // avoid mem leak by telling hib to remove that @Entity from the PersistenceContext
-             .map(o -> o.getId() + ";" + o.getCreationDate() + "\n")
-             .forEach(Unchecked.consumer(writer::write)) // org.jooq:jool
-//             .forEach(UtilFPShmecher.higherOrder(writer::write))
-         ;
-
-
+         contentWriteFunction.accept(writer);
          System.out.println("File export completed: " + file.getAbsolutePath());
       } catch (Exception e) {
          System.out.println("Imagine: Send Error Notification Email");
          throw new RuntimeException("Error exporting data", e);
       } finally {
-         System.out.println("Export finished in: " + (System.currentTimeMillis()-t0));
+         System.out.println("Export finished in: " + (System.currentTimeMillis() - t0));
       }
    }
-
+}
+@Service
+class OrderContentWriter {
+   @Autowired
+   private OrderRepo orderRepo;
+//   @SneakyThrows // pacaleste javac sa nu mai tipe la unhandled checked exceptions
+   public void writeContent(Writer writer) throws IOException{
+      writer.write("OrderID;Date\n");
+      orderRepo.findByActiveTrue()
+          .map(o -> o.getId() + ";" + o.getCreationDate() + "\n")
+          .forEach(Unchecked.consumer(writer::write)); // org.jooq:jool
+   }
+}
+@Service
+class UserContentWriter {
+   @Autowired
+   private UserRepo userRepo;
+//   @SneakyThrows // pacaleste javac sa nu mai tipe la unhandled checked exceptions
+   public void writeContent(Writer writer) throws IOException{
+      writer.write("userid;fullname\n");
+      userRepo.findAll().stream()
+          .map(u -> u.getId() + ";" + u.getFullName() + "\n")
+          .forEach(Unchecked.consumer(writer::write)); // org.jooq:jool
+   }
 }
 class UtilFPShmecher {
 
@@ -81,8 +95,12 @@ public class LoanPattern implements CommandLineRunner {
    }
 
    private final FileExporter fileExporter;
+   private final OrderContentWriter orderContentWriter;
+   private final UserContentWriter userContentWriter;
    public void run(String... args) throws Exception {
-      fileExporter.exportFile();
+      fileExporter.exportFile("orders.csv", Unchecked.consumer(orderContentWriter::writeContent));
+
+      fileExporter.exportFile("users.csv", Unchecked.consumer(userContentWriter::writeContent));
 
       // TODO implement export of users too
    }
